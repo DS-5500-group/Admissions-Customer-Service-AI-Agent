2c6bb1cc88fca1f1410b75e3476e311197e893f6
Aidan <87510335+CowAvenge@users.noreply.github.com>
2026-02-08 14:45:38 -0500

    Add files via upload

---
+++ DatabaseSchema/DatabaseTemplate.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE CapstoneDB;
+
+Use CapstoneDB;
+
+#This Table will hold information for the University
+Create Table InformationDatabase(
+id INT AUTO_INCREMENT PRIMARY KEY,
+Category Varchar(255),
+Sub_Category Varchar(255),
+Information_Text Text,
+Source_Name Varchar(1028)
+);
+
+#This table will hold the User Question, Agent Answer, and Agent confidence on answer.
+Create Table AnswerConfidence(
+Question Varchar(255),
+Answer Varchar(1028),
+confidenceLevel INT
+);
+
